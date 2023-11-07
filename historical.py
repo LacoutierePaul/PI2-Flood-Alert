34dcd7ed9f077c7522cd3c7d50458fbff3460ca7
LacoutierePaul <144808507+LacoutierePaul@users.noreply.github.com>
2023-11-07 15:28:12 +0100

    Create historical.py

---
+++ historical.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import pandas as pd
+url = 'https://environment.data.gov.uk/flood-monitoring/data/readings.csv'
+params = {
+    'date': '2023-01-01',
+}
+df = pd.read_csv(url, parse_dates=['dateTime'])
+
+df.head(10)
