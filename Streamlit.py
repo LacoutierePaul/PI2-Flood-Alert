443f549d9178351172ab73a9ce66ad4c247071e4
LacoutierePaul <paul.lacoutiere@gmail.com>
2023-12-02 19:20:00 +0100

    Merge Loevan + ajout recherche station

--- Streamlit.py
+++ Streamlit.py
@@ -1,4 +1,6 @@
 #Code
+# To run the Streamlit web app type: python -m streamlit run Streamlit.py or streamlit run Streamlite.py
+
 
 import requests
 import streamlit as st
@@ -8,15 +10,16 @@ import pandas as pd
 import folium
 import geopandas as gpd
 
-st.write(""" # Pi² 
-          Premier Test Streamlit Innondation""")
-date=st.date_input("Pick a date")
-st.write("You pick : ",date)
+# ...
+
+st.write(""" # Pi² Diot-Siaci""")
+date = st.date_input("Pick a date")
+st.write("You pick : ", date)
 
 url = "http://environment.data.gov.uk/flood-monitoring/id/floods"
 params = {
     #'date': date,
-    "min-severity" : 3
+    "min-severity": 3
 }
 
 try:
@@ -29,48 +32,56 @@ df = pd.DataFrame(data["items"])
 columns_to_drop = ['eaRegionName', 'isTidal', 'timeSeverityChanged', 'timeMessageChanged']
 df['floodArea'] = df['floodArea'].apply(lambda x: x['polygon'] if 'polygon' in x else None)
 df = df.drop(columns=[col for col in columns_to_drop if col in df.columns])
-# affichage des alertes sur la derniere heure
+# affichage des alertes sur la dernière heure
 df['timeRaised'] = pd.to_datetime(df['timeRaised'])
 df['timeRaised'] = df['timeRaised'].dt.date
-st.dataframe(df)
+#st.dataframe(df)
 
 filtered_df = df[df['timeRaised'] == date]
+
 # Créer une carte Folium ou tout autre élément de votre application Streamlit
 # Afficher le DataFrame filtré dans votre application
 st.title(f'Données pour la date {date}')
 filtered_df = filtered_df.sort_values(by='severityLevel')
-st.dataframe(filtered_df)
 
+# Définissez le style en fonction du niveau de gravité à l'extérieur de la boucle
+def get_style(severity_level):
+    if severity_level == 4:
+        return {'fillColor': 'green', 'color': 'green'}
+    elif severity_level == 3:
+        return {'fillColor': 'yellow', 'color': 'yellow'}
+    elif severity_level == 2:
+        return {'fillColor': 'orange', 'color': 'orange'}
+    elif severity_level == 1:
+        return {'fillColor': 'red', 'color': 'red'}
+
+# Créez la carte une seule fois
 m = folium.Map(location=[53.5, -1.5], zoom_start=6)
 
-for x in filtered_df['floodArea'].head(10):
-    # fichier geojson contenant le polygon
-    geojson_file = x
-    gdf = gpd.read_file(geojson_file)
+# Utilisez iterrows pour itérer sur les lignes du DataFrame
+for index, row in filtered_df.head(10).iterrows():
+    geojson_file = row['floodArea']
+    
+    # Ajoutez le polygon avec le style correspondant
+    folium.GeoJson(gpd.read_file(geojson_file), style_function=lambda x: get_style(row['severityLevel'])).add_to(m)
+   
+# Ajoutez des onglets
+tabs = ["Dataframe", "Map","Select by City"]
+selected_tab = st.sidebar.radio("Navigation", tabs)
 
-    # ajout du polygon sur la carte avec la couleur correspondant au niveau d'alerte (jaune ou rouge) avec bordure de la même couleur
-    if filtered_df['severityLevel'].iloc[0] == 4:
-        folium.GeoJson(gdf, style_function=lambda x: {'fillColor': 'green', 'color': 'green'}).add_to(m)
-    elif filtered_df['severityLevel'].iloc[0] == 3:
-        folium.GeoJson(gdf, style_function=lambda x: {'fillColor': 'yellow', 'color': 'yellow'}).add_to(m)
-    elif filtered_df['severityLevel'].iloc[0] == 2:
-        folium.GeoJson(gdf, style_function=lambda x: {'fillColor': 'orange', 'color': 'orange'}).add_to(m)
-    elif filtered_df['severityLevel'].iloc[0] == 1:
-        folium.GeoJson(gdf, style_function=lambda x: {'fillColor': 'red', 'color': 'red'}).add_to(m)
-    else:
-        folium.GeoJson(gdf).add_to(m)
-
-
-st.title('Carte Folium dans Streamlit')
-folium_static(m)
-
-col1, col2 = st.columns(2)
-
-# Placez des éléments dans chaque colonne
-with col1:
+if selected_tab == "Dataframe":
+    # Affichez le DataFrame dans le premier onglet
     st.header("Dataframe")
     st.dataframe(filtered_df)
 
-with col2:
+elif selected_tab == "Map":
+    # Affichez la carte Folium dans le deuxième onglet
     st.header("Map")
     folium_static(m)
+
+elif selected_tab == "Select by City":
+    # Possibilité de filtrer sur une ville le df
+    st.write( """## Filtrer sur une ville""")
+    cities=df['eaAreaName'].unique()
+    choix_utilisateur = st.selectbox('Sélectionnez une ville', cities)
+    st.dataframe(df[df['eaAreaName']==choix_utilisateur])
--- Streamlit2.py
+++ Streamlit2.py
@@ -1,87 +1,179 @@
-#Code
-# To run the Streamlit web app type: python -m streamlit run Streamlit.py or streamlit run Streamlite.py
-
+# to run the Streamlit web app type: streamlit run *.py
 
 import requests
 import streamlit as st
-from streamlit_folium import folium_static
 import json
 import pandas as pd
 import folium
-import geopandas as gpd
+from folium.plugins import MarkerCluster
+from streamlit_folium import folium_static
+from folium import Circle
+from folium import plugins
 
-# ...
+def create_map(latitude, longitude, our_radius):
+    m = folium.Map(location=[latitude, longitude], zoom_start=6)
+
+    # Add a marker for the selected point
+    folium.Marker([latitude, longitude], popup="Point Sélectionné").add_to(m)
+
+    # Add a circle around the selected point
+    folium.Circle(
+        location=[latitude, longitude],
+        radius=our_radius * 1000,
+        color="blue",
+        fill=True,
+        fill_color="blue",
+        fill_opacity=0.2,
+        popup=f"Rayon du cercle: {our_radius} mètres"
+    ).add_to(m)
+
+    # Display the map in Streamlit
+    url_stations = "http://environment.data.gov.uk/flood-monitoring/id/stations"
+
+    params_stations2 = {
+        'lat': latitude,
+        'long': longitude,
+        'dist': our_radius
+    }
+
+    try:
+        api_request = requests.get(url_stations, params=params_stations2)
+        data_stations = json.loads(api_request.content)
+    except Exception as e:
+        data_stations = "Error..."
+
+    df2 = pd.DataFrame(data_stations['items'])
+    df2 = df2[['lat', 'long', 'stationReference']]
+    df2.dropna(subset=['lat', 'long'], inplace=True)
+    # on supprime toutes les lignes avec des valeurs sous forme de liste dans les colonnes 'lat' et 'long'
+    df2 = df2[~df2['lat'].apply(lambda x: isinstance(x, list))]
+
+    marker_cluster = MarkerCluster().add_to(m)
+
+    for index, row in df2.iterrows():
+        folium.Marker([row['lat'], row['long']], popup=row['stationReference']).add_to(marker_cluster)
+
+    folium_static(m)
+
 
 st.write(""" # Pi² Diot-Siaci""")
 date = st.date_input("Pick a date")
-st.write("You pick : ", date)
+st.write("You picked : ", date)
 
-url = "http://environment.data.gov.uk/flood-monitoring/id/floods"
-params = {
-    #'date': date,
-    "min-severity": 3
+# formattage de la date pour les paramètres de l'API
+date_params = date.strftime("%Y-%m-%d")
+
+url_readings = "http://environment.data.gov.uk/flood-monitoring/data/readings"
+url_stations = "http://environment.data.gov.uk/flood-monitoring/id/stations"
+
+params_readings = {
+    #"latest": "true",
+    #"today": "true",
+    "date": date_params,
+    #"since": "2023-01-01",
+    "_limit": "10000",
 }
-
+params_stations = {
+}
 try:
-    api_request = requests.get(url, params=params)
-    data = json.loads(api_request.content)
+    api_request = requests.get(url_readings, params=params_readings)
+    data_readings = json.loads(api_request.content)
 except Exception as e:
-    data = "Error..."
+    data_readings = "Error..."
+try:
+    api_request = requests.get(url_stations, params=params_stations)
+    data_stations = json.loads(api_request.content)
+except Exception as e:
+    data_stations = "Error..."
 
-df = pd.DataFrame(data["items"])
-columns_to_drop = ['eaRegionName', 'isTidal', 'timeSeverityChanged', 'timeMessageChanged']
-df['floodArea'] = df['floodArea'].apply(lambda x: x['polygon'] if 'polygon' in x else None)
-df = df.drop(columns=[col for col in columns_to_drop if col in df.columns])
+df = pd.DataFrame(data_readings['items'])
+df_stations = pd.DataFrame(data_stations['items'])
+
+# on ne garde que les colonnes 'lat', 'long' et 'stationReference'
+df_stations = df_stations[['lat', 'long', 'stationReference']]
+
+# ajout des colonnes 'parameter' et 'period' à partir de la colonne 'measure' (tout est dans l'URL) en utilisant regex
+df[['stationReference', 'parameter', 'qualifier', 'period', 'unitName']] = df['measure'].str.extract(r'measures/(.*)-(.*)-(.*)-.-(.*)-(.*)')
+# on supprime la colonne 'measure'
+df = df.drop('measure', axis=1)
+
+# on fusionne les deux dataframes
+df = pd.merge(df, df_stations, on='stationReference')
+
+# on supprime toutes les lignes avec une valeur manquante dans les colonnes 'lat' et 'long'
+df.dropna(subset=['lat', 'long'], inplace=True)
+# on supprime toutes les lignes avec des valeurs sous forme de liste dans les colonnes 'lat' et 'long'
+df = df[~df['lat'].apply(lambda x: isinstance(x, list))]
+
+# on supprime toutes les lignes avec comme paramètre autre que 'level'
+df = df[df['parameter'] == 'level']
+
+# trie par 'dateTime' dans l'ordre décroissant
+df.sort_values(by=['dateTime'], ascending=False, inplace=True)
+
+# on split la colonne 'dateTime' en deux colonnes 'date' et 'time'
+df[['date', 'time']] = df['dateTime'].str.split('T', expand=True)
 # affichage des alertes sur la dernière heure
-df['timeRaised'] = pd.to_datetime(df['timeRaised'])
-df['timeRaised'] = df['timeRaised'].dt.date
-#st.dataframe(df)
+df['dateTime'] = pd.to_datetime(df['dateTime'])
+df['dateTime'] = df['dateTime'].dt.date
 
-filtered_df = df[df['timeRaised'] == date]
+df_filtered = df[df['dateTime'] == date]
 
-# Créer une carte Folium ou tout autre élément de votre application Streamlit
-# Afficher le DataFrame filtré dans votre application
-st.title(f'Données pour la date {date}')
-filtered_df = filtered_df.sort_values(by='severityLevel')
+# on crée une carte centrée sur le Royaume-Uni
+m = folium.Map(location=[54.5, -3.5], zoom_start=6)
+# on ajoute les points
+marker_cluster = MarkerCluster().add_to(m)
+for index, row in df.iterrows():
+    folium.Marker([row['lat'], row['long']], popup=row['stationReference']).add_to(marker_cluster)
 
-# Définissez le style en fonction du niveau de gravité à l'extérieur de la boucle
-def get_style(severity_level):
-    if severity_level == 4:
-        return {'fillColor': 'green', 'color': 'green'}
-    elif severity_level == 3:
-        return {'fillColor': 'yellow', 'color': 'yellow'}
-    elif severity_level == 2:
-        return {'fillColor': 'orange', 'color': 'orange'}
-    elif severity_level == 1:
-        return {'fillColor': 'red', 'color': 'red'}
-
-# Créez la carte une seule fois
-m = folium.Map(location=[53.5, -1.5], zoom_start=6)
-
-# Utilisez iterrows pour itérer sur les lignes du DataFrame
-for index, row in filtered_df.head(10).iterrows():
-    geojson_file = row['floodArea']
-    
-    # Ajoutez le polygon avec le style correspondant
-    folium.GeoJson(gpd.read_file(geojson_file), style_function=lambda x: get_style(row['severityLevel'])).add_to(m)
-   
-# Ajoutez des onglets
-tabs = ["Dataframe", "Map","Select by City"]
+# ajout des onglets
+tabs = ["DataFrame", "Map", "Select by station","Find a station"]
 selected_tab = st.sidebar.radio("Navigation", tabs)
 
-if selected_tab == "Dataframe":
-    # Affichez le DataFrame dans le premier onglet
-    st.header("Dataframe")
-    st.dataframe(filtered_df)
+if selected_tab == "DataFrame":
+    # afficher le DataFrame dans le premier onglet
+    st.header("DataFrame")
+    st.write(date)
+    st.dataframe(df_filtered)
 
 elif selected_tab == "Map":
-    # Affichez la carte Folium dans le deuxième onglet
+    # afficher la carte Folium dans le deuxième onglet
     st.header("Map")
     folium_static(m)
 
-elif selected_tab == "Select by City":
-    # Possibilité de filtrer sur une ville le df
-    st.write( """## Filtrer sur une ville""")
-    cities=df['eaAreaName'].unique()
-    choix_utilisateur = st.selectbox('Sélectionnez une ville', cities)
-    st.dataframe(df[df['eaAreaName']==choix_utilisateur])
+elif selected_tab == "Select by station":
+    # possibilité de filtrer sur une station
+    st.write( """## Filtrer sur une station""")
+    cities=df['stationReference'].unique()
+    choix_utilisateur = st.selectbox('Sélectionnez une station', cities)
+    st.dataframe(df[df['stationReference']==choix_utilisateur])
+    st.write("You picked : ", choix_utilisateur)
+    import requests
+
+    url = f"{url_stations}/{choix_utilisateur}"
+    st.write(url)
+    response = requests.get(url)
+    data = response.json()
+    try:
+# Accéder à la clé "typicalRangeHigh" et "typicalRangeLow" dans la clé "stageScale"
+        typical_range_high = data["items"]["stageScale"]["typicalRangeHigh"]
+        typical_range_low = data["items"]["stageScale"]["typicalRangeLow"]
+        st.write("Typical Range High:", typical_range_high)    
+        st.write("Typical Range Low:", typical_range_low)
+    except Exception as e :
+        st.write("error")
+
+elif selected_tab=="Find a station":
+    st.title("Sélection d'une Zone sur la Carte")
+
+    # Two input fields for latitude and longitude
+    latitude = st.number_input("Latitude:", value=54.5)
+    longitude = st.number_input("Longitude:", value=-3.5)
+
+    # Range slider for adjusting the radius of the circle
+    our_radius = st.slider("Rayon du cercle (kilomètres):", min_value=1, max_value=100, value=50)
+
+    # Load Map button
+    if st.button("Load Map"):
+        create_map(latitude, longitude, our_radius)
+
